ea53ac2b885648fa3dd90b81e9b646c151f584a9
One-Solutions-2024 <onesolutions06092024@gmail.com>
2024-10-14 14:40:48 +0530

    first commit

---
+++ frontend/src/jobsData.js
@@ -0,0 +1,188 @@
+const jobsData = [
+
+
+    {
+        companyname: "Mitsogo",
+        title: "Product Support Engineer",
+        description: "B.E/B.Tech , 2024 and 2023 passed out, Flexible to work in shifts, including a willingness to work on a 24/5 rotational basis., Strong analytical and problem-solving skills., Excellent communication skills and a passion for customer service.,Basic knowledge of multiple platforms (Windows/Mac/iOS/Android) is a plus, Location: Chennai (Work from Office)",
+        apply_link: "https://forms.office.com/pages/responsepage.aspx?id=KqbnxBjPTUqTeDaRUh_kTkpZb4Wf4idCqyoir4p7sZ5UQlFTR0tEM1dCRVRKSjdYVE5YV0pMWkRJUy4u&route=shorturl",
+        image_link: "https://media.glassdoor.com/l/fb/32/a8/54/mitsogo-reception-wall.jpg"
+    },
+    {
+        companyname: "Stripe",
+        title: "Software Engineer, Intern",
+        description: "Bachelors/Masters degree/PhD 2025 | 2026,At least 2 years of university education or equivalent work experience, Some experience and familiarity with programming either through side projects or classwork., We work mostly in Java/ Ruby/ JavaScript/ Scala and Go., We believe new programming languages can be learned if the fundamentals and general knowledge are present, Experience from previous internships or other multi-person projects including open source contributions that demonstrate evaluating and receiving feedback from mentors peers and stakeholders, Familiarity with navigating and managing your work in new code bases with multiple languages, Location: Bengaluru",
+        apply_link: "https://stripe.com/jobs/listing/software-engineer-intern/6109583/apply?gh_src=73vnei",
+        image_link: "https://th.bing.com/th/id/OIP.xJ9kBm4ui7x7ZdRQLy8yrAHaE8?pid=ImgDet&w=474&h=316&rs=1"
+    },
+    {
+        companyname: "Wipro",
+        title: "Trainee/Manual Testing.",
+        description: "B.Tech / BE (CS, IT and ECE) / MCA only. 2024 pass out only.,Excellent communication skills (Oral written and listening ability)., Knowledge of Software Development Life Cycle (SDLC) principles/concepts., Principles of automation testing and some exposure might be desirable., Knowledge of system testing and software quality assurance best practices and methodologies., Ability to break down a complex problem into smaller more manageable pieces and able to understand and describe the relationships between those pieces (i.e. good analytical skills)., Apply basic relational database concepts (e.g. table relationships keys SQL and DB2 queries etc)., Experience with systems implementations, Writes intermediate SQL queries, Troubleshoots intermediate system defects and errors, Effectively manage tasks, prioritizes and proactively update status to Managers., Should be able to tie test plans to real time customer impact, Good Understanding of Data Analysis, Analyzes and debug issues, Utilizes relevant problem-solving resources as needed, Location: Bengaluru",
+        apply_link: "https://careers-wipro.icims.com/jobs/3111385/login?_jsqid=fea9dbfe-9b0b-4ccb-b99e-4c6c740e03a4&_sp=8f66a2e6-1c42-41c2-876f-ffc42f934bf9.1728280001877&_ga=2.10040498.649037331.1728279572-2118722387.1725185858&_gl=1*ag753p*_ga*MjExODcyMjM4Ny4xNzI1MTg1ODU4*_ga_5Y2BYGL910*MTcyODI3OTU3Mi4zLjEuMTcyODI3OTk4OS4zMy4wLjA.&mobile=false&width=1150&height=500&bga=true&needsRedirect=false&jan1offset=330&jun1offset=330",
+        image_link: "https://bl-i.thgim.com/public/incoming/y22soj/article66785980.ece/alternates/LANDSCAPE_1200/IMG_BL24_States_wipro_2_1_K9ATO970.jpg"
+    },
+    {
+        companyname: "COGNIZANT",
+        title: "IS MASS HIRING : ENGINEER TRAINEE.",
+        description: "Degree in B.E/B.Tech of any branch (UG full time degree).,2024 batch can apply., At the time of registration consistent academic record of a minimum of 50% in X /XII/ Diploma/ UG & PG with no standing arrears in current education., At the time of joining all recruits need to have a minimum 50% aggregate in the pursuing degree with no standing arrears., Compensation – INR 4 00 000 per annum., Night shift allowance will be paid by the project.,Work Location : Bengaluru / Chennai / Coimbatore.",
+        apply_link: "https://app.joinsuperset.com/join/#/signup/student/jobprofiles/b643e525-c83b-4505-9a10-a666be44f249",
+        image_link: "https://static.toiimg.com/thumb/msid-112500237,width-1280,height-720,resizemode-4/112500237.jpg"
+    },
+    {
+        companyname: "Capgemini",
+        title: "Software Engineer",
+        description: "BE/BTech/ ME/MTech / MCA & MSC, Freshers 2023 & 2024, Communication: You should possess strong communication skills for effective interactions with project partners., Collaboration: You should demonstrate good interpersonal and collaboration skills, Flexibility: You should be willing to skill-up in multiple technologies and work in any Capgemini city location and travel as required., Initiative: You should be able to work independently, take initiative and manage a variety of activities concurrently.,Other Skills: Good analytical and problem-solving skill.,Location: Mumbai, Pune, Bangalore",
+        apply_link: "https://app.joinsuperset.com/join/#/signup/student/jobprofiles/6e4f8e33-c0a0-4348-83af-66cd8aa8ff9e",
+        image_link: "https://sightsinplus.com/wp-content/uploads/2022/08/Capgemini-India-is-hiring-across-the-country-check-details-here.jpg"
+    },
+    {
+        companyname: "Simplotel Technologies",
+        title: "Data Automation Analyst",
+        description: "Bachelor of Engineering required. MBA is a plus., 0-3 years of experience with scripts, data warehousing, and / or automation,  Hands-on experience (or proficiency with SQL and Python), Proficiency in data mining, mathematics, and statistical analysis, Keen attention to detail to ensure accuracy in data processing, analysis, and reporting , Strong analytical and problem-solving skills,Good communication and interpersonal skills.,Ability to work effectively within a team and collaborate with cross-functional departments,Experience in data reporting and proficiency in SQL are advantageous for querying and managing relational databases.,Location:Bangalore",
+        apply_link: "https://app.joinsuperset.com/join/#/signup/student/jobprofiles/6e4f8e33-c0a0-4348-83af-66cd8aa8ff9e",
+        image_link: "https://ratetiger.com/wp-content/uploads/2021/07/simplotel-PR.jpg"
+    },
+    {
+        companyname: "Genpact",
+        title: "Technical Associate",
+        description: "BE/ BTech/ MCA, 2018/ 2019/ 2020/ 2021/ 2022/ 2023/ 2024, Experience with writing clean code in Java, Excellent oral and written communication skills  , Strong knowledge of technology security controls (Authentication, Authorization and Encryption of data, Single Sign On, Data retention/deletion etc.), Understanding of PII data and applicable controls to safeguard it., Good analytical skills , Proficient in PowerPoint and Excel",
+        apply_link: "https://genpact.taleo.net/careersection/sgy_external_career_section/jobdetail.ftl?job=ITO083797",
+        image_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbaYaam0-ve4j-3cnccZ7akzMNwmGoF2e8NA&s"
+    },
+    {
+        companyname: "Zebra",
+        title: "SOFTWARE ENGINEER",
+        description: "BTech/ MTech, Freshers(0-2yrs), Establishes requirements for less complex design projects, Works on Completing all phases of Software engineering design projects, Works on analysis of processes and delivers results to necessary stakeholder, Strong Works with latest technologies and new approaches, Multi-threading, Multi process handling and Memory management., Work Location : Pune.",
+        apply_link: "https://careers.zebra.com/careers/job/343622504714?domain=zebra.com",
+        image_link: "https://fortune.com/img-assets/wp-content/uploads/2024/05/GPTW-2024-Zebra-Headquarters-Press-Release.jpg"
+    },
+    {
+        companyname: "WIPRO",
+        title: "Data Engineer",
+        description: "BE/BTech/MCA, Freshers 2023 and 2024., Experience with Python or Java (with SQL) would be preferred, Should have some exposure to Big Data technologies like Hadoop, Spark, Kafka, etc., Knowledge of Data warehousing concepts and cloud technologies, Location: Pune.",
+        apply_link: "https://careers.wipro.com/workarea/JobDetails.aspx?id=49890",
+        image_link: "https://www.wipro.com/content/dam/nexus/en/newsroom/2022/march/Blue-collar-workers-at-Wipro-IMG-1.jpg"
+    },
+    {
+        companyname: "TCS",
+        title: "Trainee Engineer",
+        description: "B.E / B.Tech / M.E / M.Tech (All branches except Bio-Tech / Bio-Medical), 2023-2024 batch only, Freshers, Good communication skills, Basic knowledge of programming, Understanding of SDLC, Location: Pan India.",
+        apply_link: "https://www.tcs.com/careers",
+        image_link: "https://www.careers.tcs.com/Content/assets/images/tdc_2021_home_v5.png"
+    },
+    {
+        companyname: "Cognizant",
+        title: "Data Engineer",
+        description: "Full Time B.E/B.Tech/MCA/B.Sc/M.Sc in relevant streams of Computer Science., 2024-2025 graduates are preferred., Good knowledge of Python, SQL, and NoSQL databases., Experience with data analysis and data processing is a plus., Location: Pune",
+        apply_link: "https://cognizant.wd5.myworkdayjobs.com/en-US/cognizant/job/Pune/Data-Engineer--Graduate-2024-2025--Entry-Level--Full-Time_4055547213-1",
+        image_link: "https://www.cognizant.com/content/dam/cognizant/images/about-us/careers/careers-slide-2.jpg"
+    },
+    {
+        companyname: "Trimble",
+        title: "Customer Support",
+        description: "Any Graduate, Freshers(0-1yrs), Process-oriented with high attention to detail, Basic knowledge of computer MS Windows & MS Office, Problem solving and analytical skills, Excellent written and verbal communication skills, Basic knowledge of Google Workspace, Work Location: Chennai, India.",
+        apply_link: "https://careers.zebra.com/careers/job/343622504714?domain=zebra.com",
+        image_link: "https://bsmedia.business-standard.com/_media/bs/img/article/2022-03/30/full/20220330181036.jpg"
+    },
+    {
+        companyname: "Cognizant",
+        title: "Associate - Projects",
+        description: "Possess a strong background in SAP EWM Testing, Demonstrate specialized skills in EWM, Exhibit excellent problem-solving abilities, Show proficiency in developing test plans, Have experience in defect tracking and resolution, Display strong communication skills, Be familiar with industry standards, Mentor and guide junior team members, Work Location: Hybrid.",
+        apply_link: "https://cognizant.taleo.net/careersection/Lateral/jobapply.ftl?job=00060120911&lang=en&source=CWS-13082&src=CWS-13082",
+        image_link: "https://static.toiimg.com/thumb/msid-112500237,width-1280,height-720,resizemode-4/112500237.jpg"
+    },
+    {
+        companyname: "Razor Infotech",
+        title: "Python and MERN Stack Developer",
+        description: "Bachelor’s degree in Computer Science Information Technology or a related field, 2 - 3 LPA, Minimum of 1 year of professional experience in Python programming, Hands-on experience with the MERN stack: MongoDB, Express.js, React.js, Node.js, Familiarity with front-end technologies such as HTML5, CSS3, and JavaScript, Experience with version control systems, particularly Git, Work Location: In Office Delhi.",
+        apply_link: "https://unstop.com/competitions/1164238/register",
+        image_link: "https://d8it4huxumps7.cloudfront.net/uploads/images/150x150/66ed1954a4079_organisation_image-UuScTTlJli1498478550WQzm7rzD0E.png?d=200x200"
+    },
+    {
+        companyname: "Intelliworkz",
+        title: "Frontend Developer",
+        description: "Bachelor’s degree in Computer Science Information Technology or a related field, Strong knowledge of HTML5 and CSS3 structure elements tags and attributes, Experience with media queries for responsiveness, Optimizing HTML code for search engines, Familiarity with basic JavaScript, Knowledge of JavaScript libraries like jQuery, Figma to HTML design, Experience: 1 - 2 yrs, Work Location: Ahmedabad.",
+        apply_link: "https://unstop.com/competitions/1163414/register",
+        image_link: "https://images.yourstory.com/cs/images/companies/404e7f741540-925D6B6558F2414F8714EB3C0A2C5973-1660197102039.jpg?fm=auto&ar=1:1&mode=fill&fill=solid&fill-color=fff"
+    },
+    {
+        companyname: "Amazon",
+        title: "Associate",
+        description: "Any graduate, 2017-2024, Work Experience: 0 to 1 years, Excellent communication skills (written and spoken) in English language, Ability to handle and interpret large sets of data, Work Location: WFH.",
+        apply_link: "https://amazonvirtualhiring.hirepro.in/registration/incta/ju0f4/apply/?j=58164&e=14190",
+        image_link: "https://media.istockphoto.com/id/1317474419/photo/amazon.jpg?s=612x612&w=0&k=20&c=XfMWt3qTPFbhq_82ZejFTryb_v-HXRNOqxPizblgLj0="
+    },
+    {
+        companyname: "EY",
+        title: "Assurance - Associate",
+        description: "B.Com Graduates, 2021-2024, Work Experience: 0 to 1 years, Strong interpersonal and good written & oral communication skills, Robust logical and reasoning skills, Basic knowledge on MS Excel and MS Office, Interest in business and commerciality, Work Location: Noida.",
+        apply_link: "https://careers.ey.com/ey/job/Noida-Assurance-Associate-UP-201301/1120452901/",
+        image_link: "https://bsmedia.business-standard.com/_media/bs/img/article/2024-02/12/full/1707678091-5108.jpg?im=FitAndFill=(826,465)"
+    },
+    {
+        companyname: "KPMG",
+        title: "QA Engineer",
+        description: "Bachelor’s degree or equivalent experience required, No prior experience required, Study or experience focused on quality engineering, quality systems, systems monitoring, computer systems management, Ability to clearly articulate ideas, Experience with test automation frameworks such as Selenium, Cypress, or Java tools preferred, Work Location: Bangalore.",
+        apply_link: "https://unstop.com/o/QYKRL4I?utm_medium=Share&utm_source=shortUrl",
+        image_link: "https://i0.wp.com/opportunitycell.com/wp-content/uploads/2022/12/KPMG-London.png?fit=1000%2C667&ssl=1"
+    },
+    {
+        companyname: "GE",
+        title: "Software Development Engineer",
+        description: "GE Digital Career 2024 Invites Students From Various Disciplines to apply for the Position of Software Development Engineer in Hyderabad, India, Freshers/Experienced, Bachelor’s Degree in Computer Science or STEM Majors, Expertise in Java programming, Proficient in front-end technologies like Angular, HTML5, and CSS, Work Location: Hyderabad, India (Hybrid).",
+        apply_link: "https://jobs.gecareers.com/global/en/apply?jobSeqNo=GE11GLOBALR3780300EXTERNALENGLOBAL&step=1&stepname=personalInformation",
+        image_link: "https://job4software.com/wp-content/uploads/2024/09/gettyimages-868982314-scaled.webp"
+    },
+    {
+        companyname: "Amazon",
+        title: "Device Associate",
+        description: "Graduate, preferably in a quantitative field of study with relevant experience of 0-1 years, Good communication skills, detail-oriented, Knowledge of QA methodology and tools, Execute test instructions and report test results accurately and promptly.",
+        apply_link: "https://www.amazon.jobs/en/jobs/2769644/device-associate",
+        image_link: "https://etimg.etb2bimg.com/photo/91892300.cms"
+    },
+    {
+        companyname: "Sutherland",
+        title: "International Voice Process-Hyderabad",
+        description: "Must be fluent in English, Excellent communication skills can apply, Both freshers and experienced candidates can apply, Graduation is not mandatory, 24/7 shifts and rotational week offs.",
+        apply_link: "https://www.naukri.com/job-listings-hiring-freshers-experience-for-international-voice-process-hyderabad-sutherland-hyderabad-1-to-3-years-110924019859?utmcampaign=androidjd&utmsource=share&src=sharedjd",
+        image_link: "https://pbs.twimg.com/media/FNVOGCyacAAuQem.jpg"
+    },
+    {
+        companyname: "HCL",
+        title: "Hiring Email & Chat Support",
+        description: "HCL Tech Walk-In Drive 2024, Any Degree, Basic understanding of HTML, JavaScript & CSS, Knowledge of technical troubleshooting, Strong analytical skills, Willingness to work in night shifts.",
+        apply_link: "https://www.naukri.com/job-listings/hcltech-walk-in-drive-email-chat-support-14th-15th-sep-hyderabad-hcltech-hyderabad-1-to-5-years-110924020680?utmcampaign=androidjd&utmsource=share&src=sharedjd",
+        image_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt1VGT9Q_oR45MkKkJJTYd1vuxacMtSmZf1BLACRNwH38V6Okh1lc8Nh3vI1ZIa4jh9aU&usqp=CAU"
+    },
+    {
+        companyname: "KPMG",
+        title: "Data Engineer",
+        description: "Bachelor's degree in computer science or related field, Excellent communication and team collaboration skills, Experience in Data and Analytics, Strong programming skills in Python, PySpark, SQL, and experience building stream-processing applications.",
+        apply_link: "https://ejgk.fa.em2.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1/job/INTG10019786/apply/email",
+        image_link: "https://i0.wp.com/opportunitycell.com/wp-content/uploads/2022/12/KPMG-London.png?fit=1000%2C667&ssl=1"
+    },
+    {
+        companyname: "Uber",
+        title: "Data Engineer II",
+        description: "Bachelor's degree in Computer Science or related technical field, Experience coding using programming languages (e.g., Java, Python), Understanding and exposure to data tech stack, Ability to identify and resolve performance and scalability issues.",
+        apply_link: "https://www.uber.com/careers/apply/interstitial/133571?uclick_id=e180fbe2-4219-4b03-bf9d-9358d3cddb84",
+        image_link: "https://img.etimg.com/thumb/width-1200,height-1200,imgsize-40032,resizemode-75,msid-105470730/tech/technology/uber-rolls-out-rewards-programme-uber-pro-for-drivers-in-12-cities.jpg"
+    },
+    {
+        companyname: "Kreativstorm",
+        title: "Python Developer - Intern",
+        description: "Students pursuing a degree or recent graduates in Computer Science, Proficiency in Python programming, Familiarity with software development lifecycle, Work Location: Hyderabad.",
+        apply_link: "https://kreativstorm.zohorecruit.eu/jobs/Careers/69764000051925815",
+        image_link: "https://kreativstorm.zohorecruit.eu/recruit/viewCareerImage.do?page_id=69764000000401668&type=logo&file_name=256x256_transparent.png"
+    },
+    {
+        companyname: "Splunk",
+        title: "Technical Support Engineer",
+        description: "Basic understanding of networking concepts, Excellent time management skills, Develop and maintain a deep understanding of the Splunk product, Work Location: Hyderabad, India.",
+        apply_link: "https://www.splunk.com/en_us/careers/jobs/technical-support-engineer-cloud-30829",
+        image_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSBLLLkyDaLgoJoJQMyzuwPUsTt1sW5kTaow&s"
+    }
+];
+
+export default jobsData;
