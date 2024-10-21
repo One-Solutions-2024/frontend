623cf3b6a3d9e126ab7378b57d783df78e8db64f
One-Solutions-2024 <onesolutions06092024@gmail.com>
2024-10-21 20:30:45 +0530

    first commit

--- frontend/src/JobList.js
+++ frontend/src/JobList.js
@@ -368,7 +368,7 @@ function JobList() {
                           className='job-image'
                         />
                         <p className='job-description'>{job.description.slice(0, 30)}...</p>
-                        <a href="#" className="menu-item-link">
+                        <a href={`https://company-details.pages.dev/company/${job.companyname}/${job.url}`} className="menu-item-link">
                           View
                           <svg width="16px" height="16px" viewBox="0 0 16 16" className="bi bi-arrow-right" fill="#d0b200" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
