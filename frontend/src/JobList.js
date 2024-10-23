2701e0dfeee4f93bf1b771ff76e1880b55d80159
One-Solutions-2024 <onesolutions06092024@gmail.com>
2024-10-23 16:02:30 +0530

    first commit

--- frontend/src/JobList.js
+++ frontend/src/JobList.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Footer from "./components/Footer";
 import YouTubeVideos from './components/Youtube';
-import { assest } from './Images/images'; "./"
+import { assest } from './Images/images';
 import './JobList.css';
 
 function JobList() {
