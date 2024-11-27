5d1ca40d2c3a806714266ab44bebf7ae43566ad1
One-Solutions-2024 <onesolutions06092024@gmail.com>
2024-11-27 19:36:59 +0530

    first commit

--- frontend/src/components/RunningJobs/index.js
+++ frontend/src/components/RunningJobs/index.js
@@ -67,7 +67,7 @@ const RunningJobs = ({ jobs = [], handleCardClick }) => {
                     >
                         <div className="image-container">
                             <img
-                                src={job.image}
+                                src={`https://backend-vtwx.onrender.com/uploads/${image}`}
                                 alt={`Image of ${job.title}`}
                                 className="job-image-runningjob"
                             />
