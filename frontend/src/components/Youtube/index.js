0319daab3c4c15bdf2c1020d134fe8b5bbc55ddd
One-Solutions-2024 <onesolutions06092024@gmail.com>
2025-01-07 19:39:45 +0530

    first commit

--- frontend/src/components/Youtube/index.js
+++ frontend/src/components/Youtube/index.js
@@ -3,6 +3,7 @@ import './youtube.css';
 
 // List of video IDs from your provided links
 const videoList = [
+  { id: 'rsKrXeABMtI', title: '6 Exciting Job Updates: Wipro, Ags Health, Cisco, Qualcomm, Zycus and Fresh Prints | One Solutions'},
   { id: 'NaxafQ_ly58', title: '6 Exciting Job Updates : Deloite, Simens, WabTec,  EY, Motorola and HCL are Hiring | One Solutions'},
   { id: 'i2vWsNjoFto', title: '4 Exciting Job Updates : Mitsogo, Stripe, Wipro and Cognizant are Hiring | One Solutions' },
   { id: 'yavE6elZ-6E', title: 'INFOANE Company Interview Process For Freshers' },
